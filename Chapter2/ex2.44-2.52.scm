55d9f7d1c42897368684a112a5aa44abb3364830
ellipse <xdgxygpl@gmail.com>
2013-03-01 19:41:08 +0800

    completed

--- Chapter2/ex2.44-2.52.scm
+++ Chapter2/ex2.44-2.52.scm
@@ -209,7 +209,10 @@
            (transform-painter painter2
                               split-point
                               (make-vect 1.0 0.0)
-                              (make-vect 0.0 1.0)))))))
+                              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-down frame)
+        (paint-up frame)))))
 (define (below-another-version painter1 painter2)
   (rotate90
    (beside (rotate270 painter1)
