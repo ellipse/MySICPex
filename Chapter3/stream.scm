6eb4ccf3533e2c03b76a46813b308a7fd4c2b61e
ellipse <xdgxygpl@gmail.com>
2013-03-08 22:50:07 +0800

    uncompleted

---
+++ Chapter3/stream.scm
@@ -0,0 +1,10 @@
+;; primitives for constructing and operating on streams
+
+(define (memo-poc) )
+(define (delay) )
+(define (force) )
+(define (cons-stream) )
+(define (stream-car) )
+(define (stream-cdr) )
+(define (add-stream) )
+(define (map-stream) )
